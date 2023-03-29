2d49611e046b919fd1ec3128c12c66dafcbbaee2
Guilliano van Dinther <guilliano.vandinther@ictmbo.nl>
2023-03-29 15:26:32 +0200

    added events to the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use \App\Models\User;
+use App\Models\Event;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
          User::factory(10)->create();
+         Event::factory(10)->create();
     }
 }
