2ea7f34820a465dd1a74d471a23d3860dd9641bc
Frits Notenboom <teus.brom@ictmbo.nl>
2023-03-30 16:02:37 +0200

    gradient

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,7 +1,12 @@
 <!-- main app -->
 <template>
-        <router-view v-slot="{ Component }">
-            <component :is="Component">
-            </component>
-        </router-view>
+    <router-view v-slot="{ Component }">
+        <component :is="Component">
+        </component>
+    </router-view>
 </template>
+<style>
+html {
+    background: linear-gradient(90deg, rgb(107 103 180) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);
+}
+</style>
